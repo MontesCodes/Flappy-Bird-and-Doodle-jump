705f92d2c499c57bca57fa6b74ebb5fca27e83f3
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-03-01 19:41:07 -0500

    update to JavaScript 3/1/22

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const doodler = document.createElement('div');
   let doodlerLeftSpace = 50;
   let doodlerBottomSpace = 150;
+  let isGameOver = false;
+  let platformCount = 5;
 
   function createDoodler() {
     grid.appendChild(doodler);
@@ -11,7 +13,34 @@ document.addEventListener('DOMContentLoaded', () => {
     doodler.style.left = doodlerLeftSpace + 'px';
     doodler.style.bottom = doodlerBottomSpace + 'px';
   }
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement('div');
+
+      const visual = this.visual;
+      visual.classList.add('platform');
+      visual.style.left = this.left + 'px';
+      visual.style.bottom = this.bottom + 'px';
+      grid.appendChild(visual);
+    }
+  }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platformGap = 600 / platformCount;
+      let newPlatBottom = 100 + i * platGap;
+      let newPlatform = new platformCount(newPlatBottom);
+    }
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createDoodler;
+    }
+  }
+
+  // planning to attach a button to start game
   //   createDoodler();
-  // stopped at time 11:00 date 02/28/2022
-  //
 });
